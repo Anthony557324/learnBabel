c9b8690fbfae3de93d582524ed7e5ac1df01d99c
Anthony557324 <1621860255@qq.com>
2023-07-31 10:15:55 +0800

    feat:json字符串

---
+++ package.js
@@ -0,0 +1,18 @@
+const a = `{
+  "name": "learn-babel",
+  "version": "1.0.0",
+  "description": "learn babel",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo Error: no test specified && exit 1"
+  },
+  "author": "Anthony557",
+  "license": "MIT"
+}`
+// 将js的对象序列化成json字符串
+// const aStr = JSON.stringify(a)
+// console.log(aStr);
+
+// 如果你想要将json字符串转换成js对象，可以使用JSON.parse()方法
+const aObj = JSON.parse(a)
+console.log(aObj);
