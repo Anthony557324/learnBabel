47a6b7c42eb554ea24ed51818f29f6347bd06601
Anthony557324 <1621860255@qq.com>
2023-08-04 18:57:32 +0800

    feat:first commit

---
+++ main.mjs
@@ -0,0 +1,23 @@
+// import { a } from './index.mjs'
+
+// function test() {
+//   console.log('hello world')
+// }
+
+// export {
+//   test
+// }
+
+// export default test
+
+import {a} from './index.mjs'
+
+function test() {
+  console.log('hello world');
+}
+
+export {
+  test
+}
+
+export default test
--- package.js
+++ package.js
@@ -2,6 +2,7 @@ const a = `{
   "name": "learn-babel",
   "version": "1.0.0",
   "description": "learn babel",
+  "type": "module",
   "main": "index.js",
   "scripts": {
     "test": "echo Error: no test specified && exit 1"
